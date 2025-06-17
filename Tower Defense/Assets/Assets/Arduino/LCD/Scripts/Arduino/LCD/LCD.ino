61742c8ddd3a2353b16aad5fd8269cd4826636fa
kalderlunt <kalderlunt@gmail.com>
2025-06-17 03:02:27 +0200

    fix word in lcd
    display “Ready!” after 2 seconds

--- Tower Defense/Assets/Assets/Arduino/LCD/Scripts/Arduino/LCD/LCD.ino
+++ Tower Defense/Assets/Assets/Arduino/LCD/Scripts/Arduino/LCD/LCD.ino
@@ -5,22 +5,53 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 String inputString = "";
 
-void setup() {
+unsigned long lastSerialTime = 0;  // Dernière fois qu'un caractère a été reçu
+const unsigned long waitTime = 2000; // en ms
+bool readyDisplayed = false;
+bool dataReceived = false;
+
+
+void setup() 
+{
   lcd.begin(16, 2);
   lcd.print("Ready!");
   Serial.begin(9600);
+  
+  readyDisplayed = true;
 }
 
-void loop() {
-  while (Serial.available()) {
+void loop() 
+{
+  dataReceived = false;
+
+  while (Serial.available()) 
+  {
     char c = Serial.read();
-    if (c == '\n') { // Fin de ligne reçue
+    
+    lastSerialTime = millis(); // Met à jour le chrono à chaque caractère reçu
+    dataReceived = true;
+
+    if (c == '\n') // Fin de ligne
+    {
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print(inputString);
+      Serial.println(inputString);
       inputString = "";
-    } else {
+      readyDisplayed = false;
+    }
+    else 
+    {
       inputString += c;
     }
+
+  }
+  
+  if (!dataReceived && !readyDisplayed && millis() - lastSerialTime > waitTime)
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Ready!");
+    readyDisplayed = true;
   }
 }
